5ba7106433c629b5f601f3eec61018a89a9fa1c5
OmerFarukCAN <omerfarukcaan@gmail.com>
2022-10-17 23:18:40 +0300

    SQL Lesson Query

--- SQL-Lesson1.sql
+++ SQL-Lesson1.sql
@@ -43,6 +43,27 @@
 -- SELECT * FROM actor
 -- WHERE first_name = 'Penelope' AND last_name = 'Monroe' AND last_name = 'Swank' OR first_name = 'Joe';
 
+-- BETWEEN
+
+-- SELECT title, length FROM film
+-- WHERE length BETWEEN 99 AND 120; -- 99 and 120 include
+
+-- SELECT title, length FROM film
+-- WHERE length NOT BETWEEN 99 AND 120;
+
+-- SELECT rental_rate, replacement_cost FROM film
+-- WHERE (rental_rate BETWEEN 2 AND 4) AND (replacement_cost BETWEEN 10 AND 20);
+
+-- IN
+
+-- SELECT title, length FROM film
+-- WHERE length IN(40, 50, 60);
+
+-- SELECT title, length FROM film
+-- WHERE length IN(40, 50, 60) AND title IN('Adaptation Holes');
+
+-- Select replacement_cost FROM film
+-- WHERE replacement_cost NOT IN (10.99, 12.99, 16.99);
 
 
 
